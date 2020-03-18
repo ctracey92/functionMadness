b618e6f9f223fb97991a642217b54968ffbe681c
Ctracey92 <cody.tracey92@gmail.com>
2020-03-17 20:05:48 -0500

    first two test cases are working as designed, but i think there is an inifinite loop hitting starting with the 3rd test case

--- projectEuler/03-largestPrimeFactor.js
+++ projectEuler/03-largestPrimeFactor.js
@@ -7,18 +7,37 @@ const largestPrimeFactor = (num) => {
     let primeFactor = 2;
     let numLeft = num;
 
-    while(num !== 1){
+    while(numLeft > 1){
+        if(numLeft%primeFactor === 0){
+            numLeft /= primeFactor;
+            console.log("Work")
+        }
+        else{
+            let newPrimeFound = false;
+            let counter = primeFactor + 1;
+            while(newPrimeFound === false){
+                let prime = true;
+                for(let i = 2; i < counter; i++){
+                    if(counter%i === 0){
+                        prime = false;
+                    };
+                };
+                if(prime){
+                    newPrimeFound = true;
+                    primeFactor = counter;
+                };
+            }
+        
+        }
 
     }
-
-
 
     return primeFactor
 }
 
-console.log(largestPrimeFactor(2), "<- should return 2") //should return 2.
-console.log(largestPrimeFactor(3), "<- should return 3") //should return 3.
-console.log(largestPrimeFactor(5), "<- should return 5") //should return 5.
-console.log(largestPrimeFactor(7), "<- should return 7") //should return 7.
-console.log(largestPrimeFactor(13195), "<- should return 29") //should return 29.
-console.log(largestPrimeFactor(600851475143), "<- should return 6857") //should return 6857.
+// console.log(largestPrimeFactor(2), "<- should return 2") //should return 2.
+// console.log(largestPrimeFactor(3), "<- should return 3") //should return 3.
+// console.log(largestPrimeFactor(5), "<- should return 5") //should return 5.
+// console.log(largestPrimeFactor(7), "<- should return 7") //should return 7.
+// console.log(largestPrimeFactor(13195), "<- should return 29") //should return 29.
+// console.log(largestPrimeFactor(600851475143), "<- should return 6857") //should return 6857.
