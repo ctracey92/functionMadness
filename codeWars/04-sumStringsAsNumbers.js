5943f3ea7e886a71fe39b825febbabee8f649d53
Ctracey92 <cody.tracey92@gmail.com>
2020-03-23 15:32:01 -0500

    cleaned up the code, added test cases, commented it

--- codeWars/04-sumStringsAsNumbers.js
+++ codeWars/04-sumStringsAsNumbers.js
@@ -14,9 +14,13 @@ const sumStrings = (a, b) => {
   a ? (a = a) : (a = "0");
   b ? (b = b) : (b = "0");
 
+  //If the number would be shown in e notation...
   if ((Number(a) + Number(b)).toString().includes("e")) {
+    //Split the strings into arrays
     let aArr = a.split("");
     let bArr = b.split("");
+
+    //If the strings aren't the same length add 0's to the beggining of the shorter array until they match
     if (a.length > b.length) {
       while (bArr.length < aArr.length) {
         bArr.unshift("0");
@@ -27,11 +31,13 @@ const sumStrings = (a, b) => {
       }
     }
 
+    //Our array to hold the combined product of the two arrays individual values
     let combined = [];
     for (let i = 0; i < aArr.length; i++) {
       combined.push(parseInt(aArr[i]) + parseInt(bArr[i]));
     }
 
+    //Working backwards over our combined array we will check to see if the number is greater than 9, if it is subtract 10 and add one to the item to the left of it, this is so that we can join the array back together to get an accurate number
     for (let i = combined.length - 1; i > 0; i--) {
       if (combined[i] > 9) {
         combined[i] -= 10;
@@ -39,8 +45,33 @@ const sumStrings = (a, b) => {
       }
     }
 
+    //Join it and return it
     return combined.join("");
-  } else {
-    return (Number(a) + Number(b)).toString();
+  }
+  //Otherwise make them numbers, add them, and turn them into a string
+  else {
+    return (parseInt(a) + parseInt(b)).toString();
   }
 };
+
+/*
+Normally we would use BigInt to do this problem, which would shorten it down tremendously but the problem did not allow for it.
+*/
+
+//Test Cases
+console.log("123", "456", "<- Should be 579"); //Should be 579
+console.log("8797", "45", "<- Should be 8842"); //Should be 8842
+console.log("800", "9567", "<- Should be 10367"); //Should be 10367
+console.log("99", "1", "<- Should be 100"); //Should be 100
+console.log("00103", "08567", "<- Should be 8670"); //Should be 8670
+console.log("", "5", "<- Should be 5"); //Should be 5
+console.log(
+  "712569312664357328695151392",
+  "8100824045303269669937",
+  "<- Should be 712577413488402631964821329"
+); //Should be 712577413488402631964821329
+console.log(
+  "50095301248058391139327916261",
+  "81055900096023504197206408605",
+  "<- Should be 131151201344081895336534324866"
+); //Should be 131151201344081895336534324866
