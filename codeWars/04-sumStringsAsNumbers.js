29fcc0494fe8873c0ff81143d6f93e7a96047f0d
Ctracey92 <cody.tracey92@gmail.com>
2020-03-23 15:24:32 -0500

    found a solution by breaking it into an array and combining the numbers then joining it together

--- codeWars/04-sumStringsAsNumbers.js
+++ codeWars/04-sumStringsAsNumbers.js
@@ -15,50 +15,32 @@ const sumStrings = (a, b) => {
   b ? (b = b) : (b = "0");
 
   if ((Number(a) + Number(b)).toString().includes("e")) {
-    let e = (parseInt(a) + parseInt(b)).toString().split("e+");
-    let num = "1";
-    let zeros = parseInt(e[1])
-    for (let i = 0; i < zeros; i++) {
-      num += 0;
+    let aArr = a.split("");
+    let bArr = b.split("");
+    if (a.length > b.length) {
+      while (bArr.length < aArr.length) {
+        bArr.unshift("0");
+      }
+    } else if (a.length < b.length) {
+      while (bArr.length > aArr.legnth) {
+        aArr.unshift("0");
+      }
     }
-    e[0] = e[0].split("")
-    e[0][e[0].length-1] = parseInt(e[0][e[0].length-1])-1
-    e[0][e[0].length-1] = e[0][e[0].length-1].toString()
-    e[0] = e[0].join('')
 
-    let bigNum = Number(e[0]) * Number(num);
-    console.log((parseInt(a) + parseInt(b)))
+    let combined = [];
+    for (let i = 0; i < aArr.length; i++) {
+      combined.push(parseInt(aArr[i]) + parseInt(bArr[i]));
+    }
 
-    let smallerNum = ((parseInt(a) + parseInt(b)) - bigNum ) //.toString();
-    console.log(smallerNum)
+    for (let i = combined.length - 1; i > 0; i--) {
+      if (combined[i] > 9) {
+        combined[i] -= 10;
+        combined[i - 1] += 1;
+      }
+    }
 
-    let finalNum = e[0].split(".").join("") + smallerNum;
-    return finalNum;
-  }else{
-    return (Number(a) + Number(b)).toString()
+    return combined.join("");
+  } else {
+    return (Number(a) + Number(b)).toString();
   }
-
 };
-
-// const sumStringsWithBigInt = (a, b) => {
-//   a ? (a = a) : (a = "0");
-//   b ? (b = b) : (b = "0");
-//   let res = BigInt(a) + BigInt(b);
-//   let answer = res.toLocaleString();
-
-//   return answer;
-// };
-
-let res = sumStrings("712569312664357328695151392", "8100824045303269669937");
-// console.log(res)
-console.log(res, "712577413488402631964821329");
-console.log(
-  sumStrings("712569312664357328695151392", "8100824045303269669937").length,
-  "712577413488402631964821329".length
-);
-// console.log(
-//   sumStrings("50095301248058391139327916261", "81055900096023504197206408605"),
-
-// '131151201344081895336534324866'
-
-// );
