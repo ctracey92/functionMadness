1c84983834a9ba6d4559b8671b42de534d7d1f52
Ctracey92 <cody.tracey92@gmail.com>
2020-03-25 14:52:42 -0500

    have the rows and columns checking appropriately, now to work on the sub boxes

---
+++ codeWars/04-sudokuSolutionValidator.js
@@ -0,0 +1,127 @@
+/*
+Sudoku Background
+Sudoku is a game played on a 9x9 grid. The goal of the game is to fill all cells of the grid with digits from 1 to 9, so that each column, each row, and each of the nine 3x3 sub-grids (also known as blocks) contain all of the digits from 1 to 9.
+(More info at: http://en.wikipedia.org/wiki/Sudoku)
+
+Sudoku Solution Validator
+Write a function validSolution/ValidateSolution/valid_solution() that accepts a 2D array representing a Sudoku board, and returns true if it is a valid solution, or false otherwise. The cells of the sudoku board may also contain 0's, which will represent empty cells. Boards containing one or more zeroes are considered to be invalid solutions.
+
+The board is always 9 cells by 9 cells, and every cell only contains integers from 0 to 9.
+
+Examples
+validSolution([
+  [5, 3, 4, 6, 7, 8, 9, 1, 2],
+  [6, 7, 2, 1, 9, 5, 3, 4, 8],
+  [1, 9, 8, 3, 4, 2, 5, 6, 7],
+  [8, 5, 9, 7, 6, 1, 4, 2, 3],
+  [4, 2, 6, 8, 5, 3, 7, 9, 1],
+  [7, 1, 3, 9, 2, 4, 8, 5, 6],
+  [9, 6, 1, 5, 3, 7, 2, 8, 4],
+  [2, 8, 7, 4, 1, 9, 6, 3, 5],
+  [3, 4, 5, 2, 8, 6, 1, 7, 9]
+]); // => true
+validSolution([
+  [5, 3, 4, 6, 7, 8, 9, 1, 2], 
+  [6, 7, 2, 1, 9, 0, 3, 4, 8],
+  [1, 0, 0, 3, 4, 2, 5, 6, 0],
+  [8, 5, 9, 7, 6, 1, 0, 2, 0],
+  [4, 2, 6, 8, 5, 3, 7, 9, 1],
+  [7, 1, 3, 9, 2, 4, 8, 5, 6],
+  [9, 0, 1, 5, 3, 7, 2, 1, 4],
+  [2, 8, 7, 4, 1, 9, 6, 3, 5],
+  [3, 0, 0, 4, 8, 1, 1, 7, 9]
+]); // => false
+*/
+
+const validSolution = board => {
+  //Assume every board is valid until told otherwise
+  let valid = true;
+  //Check to see if any boards include a zero
+  board.map(i => {
+    if (i.includes("0")) {
+      valid = false;
+    }
+  });
+  //Kick it out if it is invalid
+  if (!valid) {
+    return valid;
+  };
+  let colCount = 0;
+  //check to make sure each column has 1-9
+  while(colCount < 9){
+    let items = board.reduce((score, i) => {score.push(i[colCount]); return score},[])
+    items.sort();
+    console.log(items)
+    for(let i = 0; i < 9; i++){
+        if(items[i] !== i+1){
+            valid = false;
+        };
+    }
+    if(!valid){return valid};
+    colCount++;
+  }
+  //Make sure every row has 1-9
+  let rowCount = 0;
+  while(rowCount < 9){
+    let items = board[rowCount].sort()
+    for(let i = 0; i < 9; i++){
+        if(items[i] !== i+1){
+            valid = false;
+            break;
+        };
+    }
+    if(!valid){return valid};
+    rowCount++;
+  }
+  //Make sure every sub-grid has 1-9
+  let boxedBoard = [];
+//   //Step 1: Get all of the rows into 3 sub rows to start forming the squares
+//   for(let i = 0; i < board.length; i++){
+//       let holder = [];
+//       let counter = 0;
+//       while(holder.length < 3){
+//           let threeNums = [];
+//         while(threeNums.length < 3){
+//             threeNums.push(board[i][counter]);
+//             counter++;;
+//         };
+//         holder.push(threeNums);
+//       }
+
+//       boxedBoard.push(holder)
+//   };
+
+  
+
+  return valid;
+};
+
+console.log(
+  validSolution([
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9]
+  ]),
+  true
+);
+
+console.log(
+  validSolution([
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 0, 3, 4, 8],
+    [1, 0, 0, 3, 4, 2, 5, 6, 0],
+    [8, 5, 9, 7, 6, 1, 0, 2, 0],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 0, 1, 5, 3, 7, 2, 1, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 0, 0, 4, 8, 1, 1, 7, 9]
+  ]),
+  false
+);
