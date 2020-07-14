f8a25e200f074c4744b3215eb6d59d95f00117de
Ctracey92 <cody.tracey92@gmail.com>
2020-07-14 08:21:44 -0500

    added in the comments for the morse decoder, and fixed the return

--- codeWars/06-decodeTheMorseCode.js
+++ codeWars/06-decodeTheMorseCode.js
@@ -1,8 +1,7 @@
 /*Description:
-Part of Series 1/3
-This kata is part of a series on the Morse code. After you solve this kata, you may move to the next one.
 
-In this kata you have to write a simple Morse code decoder. While the Morse code is now mostly superseded by voice and digital data communication channels, it still has its use in some applications around the world.
+
+In this excercise you have to write a simple Morse code decoder. While the Morse code is now mostly superseded by voice and digital data communication channels, it still has its use in some applications around the world.
 The Morse code encodes every character as a sequence of "dots" and "dashes". For example, the letter A is coded as ·−, letter Q is coded as −−·−, and digit 1 is coded as ·−−−−. The Morse code is case-insensitive, traditionally capital letters are used. When the message is written in Morse code, a single space is used to separate the character codes and 3 spaces are used to separate words. For example, the message HEY JUDE in Morse code is ···· · −·−−   ·−−− ··− −·· ·.
 
 NOTE: Extra spaces before or after the code have no meaning and should be ignored.
@@ -16,16 +15,18 @@ For example:
 decodeMorse('.... . -.--   .--- ..- -.. .')
 //should return "HEY JUDE"
 NOTE: For coding purposes you have to use ASCII characters . and -, not Unicode characters.
-
-
 */
 
 decodeMorse = function (morseCode) {
+  //Our holding array that will be our final answer
   let res = [];
+  //Break up the original string into an array so we can use array methods on it
   let code = morseCode.trim().split("   ");
+  //Map over our array of words, splitting each word into a sub array of letters, translating them and pushing the word into the res array.
   code.map((i) => {
     let holder = [];
     let letters = i.split(/\s+/);
+    //We use a switch statement here to do the actual translating.
     letters.map((a) => {
       switch (a) {
         case ".-":
@@ -123,6 +124,8 @@ decodeMorse = function (morseCode) {
     let word = holder.join("");
     res.push(word);
   });
+
+  return res.join(' ')
 };
 
 //Test Cases:
